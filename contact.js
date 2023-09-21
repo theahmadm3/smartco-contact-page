764da94d1bc2881ea537d0be9a349fdf8afc5419
Ahmad Muhammad <71970775+theahmadm3@users.noreply.github.com>
2023-09-21 12:01:08 +0100

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,39 +1,42 @@
 const contactInfo = {
-        firstName: "SmartCo Innovations Limited",
-        email: "smartcoinnovations@gmail.com",
-        phone: "+2348180800080",
-        address: "Suite BPG 6, Old Banex Plaza, Wuse, Abuja",
-        whatsapp: "+2348180800080",
-    };
+    firstName: "SmartCo Innovations Limited",
+    email: "smartcolimited@gmail.com",
+    phone: "+2348180800080",
+    address: "Suite BPG 6, Old Banex Plaza, Wuse, Abuja",
+    whatsapp: "+2348180800080",
+};
 
-    function generateVCardWithPicture() {
-        const img = new Image();
-        img.crossOrigin = "Anonymous";
-        img.src = "./smartcologo.png";
 
-        img.onload = function () {
-            const canvas = document.createElement("canvas");
-            canvas.width = img.width;
-            canvas.height = img.height;
-            const ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0, img.width, img.height);
+function generateVCard(contactInfo) {
+    return `BEGIN:VCARD
+VERSION:3.0
+FN:${contactInfo.firstName}
+EMAIL:${contactInfo.email}
+TEL:${contactInfo.phone}
+ADR:${contactInfo.address} // Address field
+X-SOCIALPROFILE;type=whatsapp:${contactInfo.whatsapp} // WhatsApp field
+END:VCARD`;
+}
 
-            const pictureData = canvas.toDataURL("image/jpeg");
 
-            contactInfo.picture = pictureData;
+function downloadVCard() {
+    const vCardData = generateVCard(contactInfo);
+    const blob = new Blob([vCardData], { type: "text/vcard" });
+    const url = URL.createObjectURL(blob);
 
-            const vCardData = generateVCard(contactInfo);
-            const blob = new Blob([vCardData], { type: "text/vcard" });
-            const url = URL.createObjectURL(blob);
+    const downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = "contact.vcf";
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
 
-            const downloadLink = document.createElement("a");
-            downloadLink.href = url;
-            downloadLink.download = "contact.vcf";
+    downloadLink.click();
 
-            // Trigger a click event on the download link
-            downloadLink.click();
-        };
-    }
+    
+    URL.revokeObjectURL(url);
+    document.body.removeChild(downloadLink);
+}
 
-    const downloadButton = document.getElementById("downloadBtn");
-    downloadButton.addEventListener("click", generateVCardWithPicture);
+
+const downloadButton = document.getElementById("downloadBtn");
+downloadButton.addEventListener("click", downloadVCard);
